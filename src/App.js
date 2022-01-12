f48509c3997dea7a465d043cc6b47dbb215f533d
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-12 18:30:01 -0500

    added template to add nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Nav from './components/Nav.js'
 
 function App() {
   return (
-    <div className="App">
-      <nav></nav>
+    <div>
+      <Nav />
       <h1>I want to make this app work so that I can watch movies!</h1>
-      <h2>Now how do I go about this</h2>
+      <h2>Now how do I go about this???</h2>
     </div>
   );
 }
