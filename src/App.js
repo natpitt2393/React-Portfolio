18939cbf7339559ea94d6fd00df397fb282eb013
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-17 10:49:02 -0500

    added more to skeleton that is app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutMe from './components/pages/AboutMe';
+import Projects from './components/pages/Projects';
+import Resume from './components/pages/Resume';
+import Contact from './components/pages/Contact';
 import './App.css';
 
 
 function App() {
     const [currentPage, setCurrentPage] = useState('About')
     const renderPage = () => {
-      if (currentPage === 'About') {
-        return <About />;
+      if (currentPage === 'AboutMe') {
+        return <AboutMe />;
       }
       else if (currentPage === 'Projects') {
         return <Projects />;
