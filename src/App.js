07a052467069828a8e9d9ac43ad64a667372594c
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-17 17:49:42 -0500

    refactored code from portfoliocontainer and added it to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from './components/Nav';
+import AboutMe from './components/pages/AboutMe';
+import Portfolio from './components/pages/Portfolio';
+import Resume from './components/pages/Resume';
+import Contact from './components/pages/Contact';
 
-const App = () => <PortfolioContainer />;
+function App() {
+  const [currentPage, setCurrentPage] = useState('AboutMe');
+
+  
+  const renderPage = () => {
+    if (currentPage === 'AboutMe') {
+      return <AboutMe />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <h1 className='jumbotron text-center text-primary'>Nat Epstein's Portfolio</h1>
+      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
 
 export default App;
