f9e4a0e75aae6faaa596740b642b23738a3dcdbf
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-17 10:32:43 -0500

    added code to App.js, basically the skeleton of this whole project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
-import Nav from './components/Nav.js'
+
 
 function App() {
-  return (
-    <div>
-      <h1 className="headerNat">Welcome to Nat Epstein's portfolio page!</h1>
-      <Nav />
-    </div>
-  );
-}
+    const [currentPage, setCurrentPage] = useState('About')
+    const renderPage = () => {
+      if (currentPage === 'About') {
+        return <About />;
+      }
+      else if (currentPage === 'Projects') {
+        return <Projects />;
+      }
+      else if (currentPage === 'Resume') {
+        return <Resume />;
+      } else {
+        return <Contact />;
+      }
+    };
+  
+    const handlePageChange = (page) => setCurrentPage(page);
+  
+    return (
+      <div>
+        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+        {renderPage()}
+      </div>
+    );
+  }
+
 
 export default App;
