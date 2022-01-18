1a3e69a25c49ab863fd7417c0940eccdd5dcc1a6
Nat Epstein <nathaniel.epstein@gmail.com>
2022-01-18 09:38:34 -0500

    added nav inside header and added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from './components/Nav';
+
 import AboutMe from './components/pages/AboutMe';
 import Portfolio from './components/pages/Portfolio';
 import Resume from './components/pages/Resume';
 import Contact from './components/pages/Contact';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
@@ -28,8 +29,7 @@ function App() {
 
   return (
     <div>
-      <h1 className='jumbotron text-center text-primary'>Nat Epstein's Portfolio</h1>
-      <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,2 +1,11 @@
 import React from 'react';
+import Nav from './Nav';
 
+export default function Header({ currentPage, handlePageChange }) {
+    return (
+        <div>
+            <h1 className='jumbotron text-center text-primary'>Nat Epstein's Portfolio</h1>
+            <Nav currentPage={currentPage} handlePageChange={handlePageChange}/>
+        </div>
+    )
+}
