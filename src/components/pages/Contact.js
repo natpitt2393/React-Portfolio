a36fc91f9d6b55a6e0b91c7d2c42022c02fae6e4
Nat Epstein <nathaniel.epstein@gmail.com>
2022-04-22 11:42:14 -0400

    changes to contact me

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,32 +2,8 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div>
-      <div class="contactMe container mt-3">
-      <h1 className="text-end px-5 mb-5 bg-danger text-white py-3">Contact Me</h1>
-        <h1>Contact me by email</h1>
-        <form class="row g-3">
-          <div class="col-md-6">
-            <label for="name" class="form-label"> Name </label>
-            <input type="text" class="form-control" id="name" required></input>
-          </div>
-
-          <div class="col-md-8">
-            <label for="email" class="form-label"> E-mail </label>
-            <input type="email" class="form-control" id="email" required></input>
-          </div>
-
-          <div class="col-md-12">
-            <label for="message" class="form-label">Message</label>
-            <textarea class="form-control" id="message" rows="5"></textarea>
-          </div>
-
-          <div class="col-md-12">
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </div>
-
-        </form>
-      </div>
+    <div className="text-center">
+     <a href="mailto:nathaniel.epstein@gmail.com">Please email!</a>
     </div>
   );
 }
