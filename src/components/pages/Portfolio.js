b605da41e27016049cd4ea53c9ed53e43bf0e752
Nat Epstein <nathaniel.epstein@gmail.com>
2022-11-09 11:24:41 -0500

    added more projects to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import EAC from '../images/MAINEAC.png';
 import dayPlanner from '../images/MAINDAY.png';
+import MM from '../images/MM.png';
 import MHT from '../images/LOGINMHT.png';
 import quiz from '../images/Screenshot-Index.png';
 import Project from './Project';
@@ -23,6 +24,30 @@ export default function Portfolio() {
                 deployedURL: "https://mental-health-tracker-ne.herokuapp.com/",
                 github: "https://github.com/natpitt2393/Mental-Health-Tracker",
                 img: MHT
+            },
+            {
+                id: 3,
+                projectName: "A Quiz about the Sopranos",
+                description: "A quiz built through Javascript about New Jersey's favorite and most infamous crime family!",
+                deployedURL: "https://natpitt2393.github.io/A-Quiz-About-The-Sopranos/",
+                github: "https://github.com/natpitt2393/A-Quiz-About-The-Sopranos",
+                img: quiz
+            },
+            {
+                id: 4,
+                projectName: "Mixology Masters",
+                description: "Mixology Masters is the perfect way to get your drink on with friends and family for any occasion! Simply sign up on our website and order the cocktail of your dreams! We also have fun facts about each of our cocktails so be sure to read those as well. At the end, check out and we'll have your drink ready for you in just a few days!",
+                deployedURL: "https://mixology-masters.herokuapp.com/",
+                github: "https://github.com/SteveKoutsodontis/Mixology-Masters",
+                img: MM
+            },
+            {
+                id: 5,
+                projectName: "Day Planner",
+                description: "This website enables users to keep track of their busy lives by entering in descriptions for what they have to do each hour.",
+                deployedURL: "https://natpitt2393.github.io/Nat-Epstein-Day-Planner/",
+                github: "https://github.com/natpitt2393/Nat-Epstein-Day-Planner",
+                img: dayPlanner
             }
         ];
 
