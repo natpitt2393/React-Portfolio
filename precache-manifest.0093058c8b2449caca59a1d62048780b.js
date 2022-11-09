0c90ebd54417eab3a97fa19a1a07f2501dc354c1
Nat Epstein <nathaniel.epstein@gmail.com>
2022-11-09 11:38:21 -0500

    Updates

---
+++ precache-manifest.0093058c8b2449caca59a1d62048780b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5e74395c51eb62bd0be",
+    "url": "/React-Portfolio/static/css/main.4dc59f82.chunk.css"
+  },
+  {
+    "revision": "e5e74395c51eb62bd0be",
+    "url": "/React-Portfolio/static/js/main.e5e74395.chunk.js"
+  },
+  {
+    "revision": "78380bced76d83ab4daf",
+    "url": "/React-Portfolio/static/css/1.2d65bb9a.chunk.css"
+  },
+  {
+    "revision": "78380bced76d83ab4daf",
+    "url": "/React-Portfolio/static/js/1.78380bce.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "a04eafe8dd3e329fda11ab4405a60bcd",
+    "url": "/React-Portfolio/static/media/NHE_pic.a04eafe8.JPG"
+  },
+  {
+    "revision": "4d2b166320cb193579f8d9a868458053",
+    "url": "/React-Portfolio/static/media/MAINEAC.4d2b1663.png"
+  },
+  {
+    "revision": "5849830584f663be7b52fe911f1d4b9e",
+    "url": "/React-Portfolio/static/media/MAINDAY.58498305.png"
+  },
+  {
+    "revision": "ba69e0d7ce80815378d973a1cfb24251",
+    "url": "/React-Portfolio/static/media/MM.ba69e0d7.png"
+  },
+  {
+    "revision": "6e29f7ec9e1f5a992123c8f67891a48c",
+    "url": "/React-Portfolio/static/media/LOGINMHT.6e29f7ec.png"
+  },
+  {
+    "revision": "dd2b81a7764372753de4d9a5e9f35817",
+    "url": "/React-Portfolio/static/media/Screenshot-Index.dd2b81a7.png"
+  },
+  {
+    "revision": "a45e10dd0da8a6057955135b7530e392",
+    "url": "/React-Portfolio/static/media/New_Nat_Epstein_Resume.a45e10dd.pdf"
+  },
+  {
+    "revision": "a72230d89f0dc9de0cf5d4439fad385e",
+    "url": "/React-Portfolio/index.html"
+  }
+];
