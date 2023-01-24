faea6a355df828ce49f0f181e4b6f0d3c991dad5
Nat Epstein <nathaniel.epstein@gmail.com>
2023-01-24 17:03:43 -0500

    added new images to redo resume page

---
+++ src/components/pages/Skills.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Resume from './Resume';
+import HTML from '../images/html.jpg';
+import CSS from '../images/css.jpg';
+import JS from '../images/js.jpg';
+import Bootstrap from '../images/bootstrap.jpg';
+import JQUERY from '../images/jquery.jpg';
+import Mongo from '../images.mongo.jpg';
+import Restfulapi from '../images/restfulapi.jpg';
+import Node from '../images/nodejs.jpg';
+import SQL from '../images/sql.jpg';
+import Express from '../images/express.jpg';
+import GraphQL from '../images/graphql.jpg';
+
+
+export default function Skills() {
+
+}
+
